914101e2451dbf683a602fdd6d237f79aa63a3cb
ggok0265 <pdk02kkw71@naver.com>
2025-02-24 14:49:33 +0900

    안쓰는 클래스 삭제

--- Dooor/src/main/java/com/example/dooor/repository/MissionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dooor.repository;
-
-import com.example.dooor.domain.QuestManagement.Mission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissionRepository extends JpaRepository<Mission, Integer> {
-//    List<Mission> findByStage_StageId(Integer stageId); // 단계 ID로 미션 목록 검색
-}
